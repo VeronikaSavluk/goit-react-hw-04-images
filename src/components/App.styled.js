cbb93f5373367da39206919b43b165888b3515e0
Veronika Savluk <s_veronica@ukr.net>
2022-10-11 18:37:32 +0300

    Create App.styled.js

---
+++ src/components/App.styled.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Section = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
+`;
+export const Gallery = styled.div`
+  display: grid;
+  justify-items: center;
+  align-items: center;
+`
